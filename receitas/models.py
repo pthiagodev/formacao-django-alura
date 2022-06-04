b534ac14e191496c51bbd2643d91cd71cb46a707
Thiago de Assis <pthiagodev@gmail.com>
2022-06-04 17:28:04 -0300

    Adiciona dunder method para retornar nome da receita

--- receitas/models.py
+++ receitas/models.py
@@ -15,3 +15,6 @@ class Receita(models.Model):
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
     publicada = models.BooleanField(default=False)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
+
+    def __str__(self):
+        return self.nome_receita
