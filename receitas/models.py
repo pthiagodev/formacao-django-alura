291b1e59ce5964ed92a1510c0e65e39325998806
Thiago de Assis <pthiagodev@gmail.com>
2022-06-04 15:46:57 -0300

    Adicionando campo foto, configuração de path de mídias e migration

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.forms import CharField
 from datetime import datetime
@@ -13,3 +14,4 @@ class Receita(models.Model):
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
     publicada = models.BooleanField(default=False)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
