1160014ae7c7905413ac7b99afb6c3bf5beb52a3
Thiago de Assis <pthiagodev@gmail.com>
2022-06-04 10:32:56 -0300

    Adiciona classe Receita ao models.py

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.forms import CharField
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_de_preparo = models.TextField()
+    tempo_de_preparo = models.IntegerField()
+    rendimento = models.TextField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data_receita = models.DateTimeField(default=datetime.now, blank=True)
