91289e4cb22ea5881477dbb44df46f7e0ddbf965
Thiago de Assis <pthiagodev@gmail.com>
2022-06-04 01:39:15 -0300

    Adicionando loop na view para mostrar receitas de forma dinâmica

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+
+    receitas = {
+        1: 'Pastel',
+        2: 'Lasanha',
+        3: 'Coxinha',
+        4: 'Pizza',
+        5: 'Esfiha',
+        6: 'Calzone'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita (request):
     return render(request, 'receita.html')
