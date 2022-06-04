30042fe5d9be00ddc0f8ba9c5293eab4d447801b
Thiago de Assis <pthiagodev@gmail.com>
2022-06-04 17:06:37 -0300

    Adicionando função Buscar receita

--- alurareceita/urls.py
+++ alurareceita/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from xml.etree.ElementInclude import include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', include('receitas.urls')),
     path('admin/', admin.site.urls),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404, render
 from .models import Receita
 
 def index(request):
-
     receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
 
     dados = {
@@ -20,3 +19,17 @@ def receita (request, receita_id):
     }
 
     return render(request, 'receita.html', receita_a_exibir)
+
+def buscar(request):
+    receitas_buscadas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            receitas_buscadas = receitas_buscadas.filter(nome_receita__icontains=nome_a_buscar)
+
+    dados = {
+        'receitas': receitas_buscadas
+    }
+
+    return render(request, 'buscar.html', dados)
