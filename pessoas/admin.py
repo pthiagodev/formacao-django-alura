f9dfa5f566651b594826c2e9903849c495b1c675
Thiago de Assis <pthiagodev@gmail.com>
2022-06-04 13:33:58 -0300

    Criando modelo de pessoas, adicionando registro e listagem no admin e primeira migration

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Pessoa
 # Register your models here.
+
+class ListandoPessoas(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email')
+    list_display_links = ('id', 'nome')
+
+admin.site.register(Pessoa, ListandoPessoas)
