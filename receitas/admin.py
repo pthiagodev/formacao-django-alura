6cb03d94831bf584c81dbf9be9b7289ec11f8c82
Thiago de Assis <pthiagodev@gmail.com>
2022-06-04 13:22:58 -0300

    Adicionando listagem de receitas no admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Receita
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_de_preparo')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 2
+
+admin.site.register(Receita, ListandoReceitas)
