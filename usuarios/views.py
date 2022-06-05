8599fb95ad21fa00e2ad233b00126b04209142bf
Thiago de Assis <pthiagodev@gmail.com>
2022-06-05 02:25:24 -0300

    Adicionando validações para cadastro de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from curses.ascii import US
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            return redirect('cadastro')
+        if not email.strip():
+            return redirect('cadastro')
+        if senha != senha2:
+            redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            redirect('cadastro')
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 def login(request):
     return render(request, 'usuarios/login.html')
