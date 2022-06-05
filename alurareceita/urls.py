1193d9fc462463997cbe56c7292c65c410e6d36d
Thiago de Assis <pthiagodev@gmail.com>
2022-06-05 00:09:49 -0300

    Alterando caminho do folder templates, adicionando templates de usuario (cadastro e login) e configurando views e menus

--- alurareceita/urls.py
+++ alurareceita/urls.py
@@ -19,10 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-import receitas
+import receitas, usuarios
 
 urlpatterns = [
     path('', include('receitas.urls')),
+    path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
