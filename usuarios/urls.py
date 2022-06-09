fdc60b205163efbda59d0e3e37ec7dd3cca0f92e
Thiago de Assis <pthiagodev@gmail.com>
2022-06-09 17:12:34 -0300

    feat: Adiciona formulário Criar receitas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('cadastro', views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('cria/receita', views.cria_receita, name='cria_receita')
 ]
